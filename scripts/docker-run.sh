e58572c3e9e57fd431d8c65090d56fed18f724f5
spurreiter <spurreiter@users.noreply.github.com>
2022-01-23 22:38:31 +0100

    chore: disable theme cache

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -36,31 +36,7 @@ docker run \
   -e DEBUG_PORT='*:8787' \
   -v ${cwd}/my-realm.json:/tmp/my-realm.json \
   -v ${cwd}/../deployments:/opt/jboss/keycloak/standalone/deployments \
+  -v ${cwd}/disable-theme-cache.cli:/opt/jboss/startup-scripts/disable-theme-cache.cli \
   -e KEYCLOAK_IMPORT=/tmp/my-realm.json \
   $IMAGE
-
-#  -v ${cwd}/configuration-13.0.1:/opt/jboss/keycloak/standalone/configuration \
-
-###
-# for templating unset cache:
-#
-# docker cp kc-ext:/opt/jboss/keycloak/standalone/configuration .
-#
-# scripts/configuration-13.0.1/standalone-ha.xml
-#   <subsystem xmlns="urn:jboss:domain:keycloak-server:1.1">
-#     ...
-#     <theme>
-#         <!--
-#         <staticMaxAge>2592000</staticMaxAge>
-#         <cacheThemes>true</cacheThemes>
-#         <cacheTemplates>true</cacheTemplates>
-#         -->
-#         <staticMaxAge>-1</staticMaxAge>
-#         <cacheThemes>false</cacheThemes>
-#         <cacheTemplates>false</cacheTemplates>
-#         <welcomeTheme>${env.KEYCLOAK_WELCOME_THEME:keycloak}</welcomeTheme>
-#         <default>${env.KEYCLOAK_DEFAULT_THEME:keycloak}</default>
-#         <dir>${jboss.home.dir}/themes</dir>
-#     </theme>
-# </subsystem>
 
